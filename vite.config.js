7bbbcfa5c72c981c7725a7412b8ad82addabd2a7
Marcel <marcelczubak25@gmail.com>
2025-07-26 19:52:18 +0100

    Troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-console.log('Vite config loaded — allowedHosts: all')
-
-// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-        external: ['react-icons/tb', 'react-icons/fa6', 'react-icons/rx', 'react-icons/io5',], 
-    },
-  },
   plugins: [react(), tailwindcss()],
   base: '/'
 })
